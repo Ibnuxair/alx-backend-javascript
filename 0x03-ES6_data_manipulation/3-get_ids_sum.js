b43a9ac212e098c9854f266277e365e652ee781b
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2024-03-12 12:47:20 +0000

    Task3

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,11 @@
+export default function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const reducer = (acc, item) => acc + item.id;
+
+  const sum = students.reduce(reducer, 0);
+
+  return sum;
+}
