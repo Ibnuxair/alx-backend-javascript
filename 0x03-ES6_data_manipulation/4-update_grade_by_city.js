b324d1c642ef7ff1514a9c852060284de5e65665
Ibrahim Uzairu <ibrahimuzairu28@gmail.com>
2024-03-12 14:17:32 +0000

    Task4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,32 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  if (!Array.isArray(newGrades)) {
+    return [];
+  }
+
+  const studentsFilter = students.filter((student) => student.location === city);
+
+  const studentsGraded = studentsFilter.map((student) => {
+    const gradeFilter = newGrades.filter(
+      (newGrade) => newGrade.studentId === student.id,
+    );
+
+    let grade;
+
+    if (gradeFilter[0]) {
+      grade = gradeFilter[0].grade;
+    } else {
+      grade = 'N/A';
+    }
+
+    return {
+      ...student,
+      grade,
+    };
+  });
+
+  return studentsGraded;
+}
